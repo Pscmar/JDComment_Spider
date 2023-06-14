02e96061cf4fd40898d0801ab0b34a9611a64a40
Pscmar <wyl_0320@163.com>
2023-06-14 17:06:21 +0800

    add laptop info of thinkpad15v and HPzhan99

--- SpiderScript.py
+++ SpiderScript.py
@@ -9,14 +9,19 @@ import json
 import csv
 import io
 
+import urllib3
+
+urllib3.disable_warnings()
+
 # 保存评论数据
 def commentSave(list_comment):
     '''
     list_comment: 二维list,包含了多条用户评论信息
     '''
-    file = io.open('data/JDComment_data.csv','w',encoding="utf-8",newline = '')
+    file = io.open('data/JDComment_ThinkPadP15v_i7T600','w',encoding="utf-8",newline = '')
+    # file = io.open('data/JDComment_HPzhan99_i7T600.csv','w',encoding="utf-8",newline = '')
     writer = csv.writer(file)
-    writer.writerow(['用户ID','评论内容','购买时间','点赞数','回复数','得分','评价时间','手机型号'])
+    writer.writerow(['用户ID','评论内容','购买时间','点赞数','回复数','评分','评价时间','产品型号'])#'用户位置',
     for i in range(len(list_comment)):
         writer.writerow(list_comment[i])
     file.close()
@@ -24,16 +29,16 @@ def commentSave(list_comment):
 
 def getCommentData(format_url,proc,i,maxPage):
     '''
-    format_url: 格式化的字符串架子，在循环中给它添上参数
+    format_url: 格式化的字符串框架，在循环中给它添上参数
     proc: 商品的productID，标识唯一的商品号
-    i: 商品的排序方式，例如全部商品、晒图、追评、好评等
+    i: 商品评论的展示，例如全部商品、晒图、追评、好评等
     maxPage: 商品的评论最大页数
     '''
     sig_comment = []
     global list_comment
     cur_page = 0
     while cur_page < maxPage:
-        cur_page += 1
+        
         # url = 'https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv%s&score=%s&sortType=5&page=%s&pageSize=10&isShadowSku=0&fold=1'%(proc,i,cur_page)
         url = format_url.format(proc,i,cur_page) # 给字符串添上参数
         try:
@@ -42,7 +47,7 @@ def getCommentData(format_url,proc,i,maxPage):
             jsonData = response.text
             startLoc = jsonData.find('{')
             #print(jsonData[::-1])//字符串逆序
-            jsonData = jsonData[startLoc:-2]
+            jsonData = jsonData[startLoc:]
             jsonData = json.loads(jsonData)
             pageLen = len(jsonData['comments'])
             print("当前第%s页"%cur_page)
@@ -52,9 +57,10 @@ def getCommentData(format_url,proc,i,maxPage):
                 boughtTime = jsonData['comments'][j]['referenceTime']#购买时间
                 voteCount = jsonData['comments'][j]['usefulVoteCount']#点赞数
                 replyCount = jsonData['comments'][j]['replyCount']#回复数目
-                starStep = jsonData['comments'][j]['score']#得分
+                starStep = jsonData['comments'][j]['score']#评分
                 creationTime = jsonData['comments'][j]['creationTime']#评价时间
-                referenceName = jsonData['comments'][j]['referenceName']#手机型号
+                # location = jsonData['comments'][j]['location']#用户位置
+                referenceName = jsonData['comments'][j]['referenceName']#笔记本型号
                 sig_comment.append(userId)#每一行数据
                 sig_comment.append(content)
                 sig_comment.append(boughtTime)
@@ -62,6 +68,7 @@ def getCommentData(format_url,proc,i,maxPage):
                 sig_comment.append(replyCount)
                 sig_comment.append(starStep)
                 sig_comment.append(creationTime)
+                # sig_comment.append(location)
                 sig_comment.append(referenceName)
                 list_comment.append(sig_comment)
                 print(sig_comment)
@@ -70,15 +77,19 @@ def getCommentData(format_url,proc,i,maxPage):
             time.sleep(5)
             cur_page -= 1
             print('网络故障或者是网页出现了问题，五秒后重新连接')
+        cur_page += 1
 
 if __name__ == "__main__":
     global list_comment
     ua=UserAgent()
-    format_url = 'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98&{0}&score={1}&sortType=5&page={2}&pageSize=10&isShadowSku=0&fold=1'
+    # thinkpadP15v_i7T600
+    format_url = 'https://api.m.jd.com/?appid=item-v3&functionId=pc_club_skuProductPageComments&client=pc&clientVersion=1.0.0&t=1686732829760&loginType=3&uuid=122270672.16803204500281358213226.1680320450.1686727382.1686730326.7&{0}&score={1}&sortType=5&page={2}&pageSize=10&isShadowSku=0&fold=1&bbtf=&shield='
+    # # HPzhan99_i7T600
+    # format_url = 'https://api.m.jd.com/?appid=item-v3&functionId=pc_club_skuProductPageComments&client=pc&clientVersion=1.0.0&t=1686732604299&loginType=3&uuid=122270672.16803204500281358213226.1680320450.1686727382.1686730326.7&{0}&score={1}&sortType=5&page={2}&pageSize=10&isShadowSku=0&fold=1&bbtf=&shield='
     # 设置访问请求头
     headers = {
     'Accept': '*/*',
-    'Host':"club.jd.com",
+    'Authority':"api.m.jd.com",
     "User-Agent":ua.random,
     'Referer':"https://item.jd.com/",
     'sec-ch-ua':"\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
@@ -86,34 +97,27 @@ if __name__ == "__main__":
     'Sec-Fetch-Dest': 'script',
     'Sec-Fetch-Mode':'no-cors',
     'Sec-Fetch-Site':'same-site',
-    'cookie':'your cookies'
+    'cookie':'__jdu=16803204500281358213226; shshshfpa=6aeff90d-1baa-510c-672e-9ae5e96ee79a-1615545459; shshshfpx=6aeff90d-1baa-510c-672e-9ae5e96ee79a-1615545459; jcap_dvzw_fp=fZdl7g6q1Ps9WwSkcjIJsc6_KgMKnmMz-PyDscKdrqvVjrU3L4u2_JIDl9h_1_bcJL8GIA==; whwswswws=; shshshfp=152866942b46e57e5069788500b03803; TrackID=1xHoWN1vj6YHJWemdayhu_ExIO5ojJGLysR7erhewv1H9qLp6ToPcDWkFXf-UK4nysSrn6BG52tDotlEbXAGO2Dwcu8yQD41pAiyzdBvkuGA; pinId=20Ng2tqKhhIdSFk2S27kurV9-x-f3wj7; areaId=1; ipLoc-djd=1-2800-55811-0; autoOpenApp_downCloseDate_jd_homePage=1686714550477_1; autoOpenApp_downCloseDate_auto=1686714567190_1800000; warehistory="100038768525,100038768525,"; __jdv=122270672%7Ciosapp%7Ct_335139774%7Cappshare%7CWxfriends%7C1686730356150; jsavif=1; __jda=122270672.16803204500281358213226.1680320450.1686727382.1686730326.7; __jdc=122270672; token=e753dfe6bcd3776bd13b3cc6614402d3,2,937072; __tk=20af941faa3ec55366036a424f1eafe3,2,937072; shshshsID=50e4ba3d5e2351ef73a78aaedee8c184_2_1686730704337; 3AB9D23F7A4B3C9B=VJQFZW3TIQKBU56EX3YIVXNUCZIC4Y5PB7QJ7O7YEJ2JCU6O3B67OMNDDBV3WHLVCVV25WNAOQGVO6SXHGUAV2ZWIQ; 3AB9D23F7A4B3CSS=jdd03VJQFZW3TIQKBU56EX3YIVXNUCZIC4Y5PB7QJ7O7YEJ2JCU6O3B67OMNDDBV3WHLVCVV25WNAOQGVO6SXHGUAV2ZWIQAAAAMIXECKZNIAAAAAD6N26XY33ZQG2YX; _gia_d=1; shshshfpb=se8OCKP6ahlSnl4%2B62bxuVg%3D%3D; __jdb=122270672.10.16803204500281358213226|7.1686730326'
     }
     #手机四种颜色对应的产品id参数
-    productid = ['productId=100006795590','136061&productId=5089275','22778&productId=5475612','7021&productId=6784504']
+    # productid = ['productId=100006795590','136061&productId=5089275','22778&productId=5475612','7021&productId=6784504']
     list_comment = [[]]
     sig_comment = []
-    for proc in productid:#遍历产品颜色
-        i = -1
-        while i < 7:#遍历排序方式
-            i += 1
-            if(i == 6):
-                continue
-             #先访问第0页获取最大页数，再进行循环遍历
-            url = format_url.format(proc,i,0)
-            print(url)
-            try:
-                response = requests.get(url=url, headers=headers, verify=False)
-                jsonData = response.text
-                startLoc = jsonData.find('{')
-                jsonData = jsonData[startLoc:-2]
-                jsonData = json.loads(jsonData)
-                print("最大页数%s"%jsonData['maxPage'])
-                getCommentData(format_url,proc,i,jsonData['maxPage'])#遍历每一页
-            except Exception as e:
-                i -= 1
-                print("the error is ",e)
-                print("wating---")
-                time.sleep(5)
-                #commentSave(list_comment)
-    print("爬取结束，开始存储-------")
+    url = format_url.format('productId=100023130207',0,0)
+    print(url)
+    try:
+        response = requests.get(url=url, headers=headers, verify=False)
+        jsonData = response.text
+        startLoc = jsonData.find('{')
+        jsonData = jsonData[startLoc:]
+        jsonData = json.loads(jsonData)
+        print("最大页数%s"%jsonData['maxPage'])
+        getCommentData(format_url,'productId=100023130207',0,jsonData['maxPage'])#遍历每一页
+    except Exception as e:
+        print("the error is ",e)
+        print("wating---")
+        time.sleep(5)
+        #commentSave(list_comment)
+    print("---爬取结束，开始存储---")
     commentSave(list_comment)
+    print("---存储完成---")
